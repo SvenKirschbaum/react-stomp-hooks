94c71801cd82c7dd56545975843132c9ab8e88f6
Jeffrey Soriano <jsoriano@okkralabs.com>
2021-09-20 18:03:19 -0400

    remove unused import

--- src/hooks/useSubscription.tsx
+++ src/hooks/useSubscription.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef } from 'react';
+import { useContext, useEffect } from 'react';
 import StompContext from '../context/StompContext';
 import { messageCallbackType, StompHeaders } from '@stomp/stompjs';
 
